ea994ede2a2f6a23d3d5e0e9fa7bdccc83baa8ec
berkeks <berkekasabah@gmail.com>
2024-08-05 22:30:15 +0300

    Add Americas component

---
+++ src/app/americas/americas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-americas',
+  standalone: true,
+  imports: [],
+  templateUrl: './americas.component.html',
+  styleUrl: './americas.component.css'
+})
+export class AmericasComponent {
+
+}
