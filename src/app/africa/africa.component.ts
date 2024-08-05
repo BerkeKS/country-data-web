6844a7af858413637616c5d0487cd16a324ff621
berkeks <berkekasabah@gmail.com>
2024-08-05 22:29:52 +0300

    Add Africa component

---
+++ src/app/africa/africa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-africa',
+  standalone: true,
+  imports: [],
+  templateUrl: './africa.component.html',
+  styleUrl: './africa.component.css'
+})
+export class AfricaComponent {
+
+}
