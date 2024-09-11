8f8b41c912746aa53939831f40ca3bb2ea3ad471
BerkeKS <berkekasabah@gmail.com>
2024-09-11 14:42:07 +0300

    Remove throw statement to access the parameter value

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './country.component.html',
   styleUrl: './country.component.css'
 })
-export class CountryComponent {
+export class CountryComponent implements OnInit {
+  country: String = ""
+  private route = inject(ActivatedRoute)
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.country = params["country"]
+    })
+  }
 }
