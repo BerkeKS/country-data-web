1477b0248bdcc3ff79abc23b6d629b798a6f46f9
BerkeKS <berkekasabah@gmail.com>
2024-09-12 20:28:23 +0300

    Add http request to get countries in Africa

--- src/app/africa/africa.component.ts
+++ src/app/africa/africa.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-africa',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './africa.component.html',
   styleUrl: './africa.component.css'
 })
-export class AfricaComponent {
+export class AfricaComponent implements OnInit {
+  private http = inject(HttpClient)
+  countries1: any = []
+  countries2: any = []
 
+  ngOnInit(): void {
+    this.getCountries()
+  }
+
+  getCountries() {
+    this.http.get("https://restcountries.com/v3.1/region/asia")
+    .subscribe((country: any) => {
+      this.countries1 = country
+      this.countries1.sort((a : any, b: any) => a.name.common.localeCompare(b.name.common));
+      if (this.countries1.length % 2 == 0) {
+        this.countries2 = this.countries1.slice((this.countries1.length / 2), this.countries1.length);
+        this.countries1 = this.countries1.slice(0, (this.countries1.length / 2));
+      } else {
+        this.countries2 = this.countries1.slice((this.countries1.length / 2 + 1), this.countries1.length);
+        this.countries1 = this.countries1.slice(0, (this.countries1.length / 2 + 1));
+      }
+    })
+  }
 }
